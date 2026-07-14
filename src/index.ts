536d2f90ac73bc0482f9c49abae269857e7b7358
cata36 <cataroitman@gmail.com>
2026-07-14 15:41:16 -0300

    final

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,63 @@
 import { cuandoPasa, enviarAlFrontend, iniciar } from './lib/ui.ts';
 import { cargarJugadores, obtenerJugadoresPorIds } from './lib/jugadores.ts';
 
+
 const ids: number[] = cargarJugadores();
 let roster: number[] = [];
 
+
 export function estaEnRoster(roster: number[], id: number): boolean {
-  let esta: boolean = false; // COMPLETAR
+  let esta = false;
+
+
+   if(roster[id] != null)
+   {
+    esta = false;
+   }
+
+
+
+
+
+
   return esta;
 }
 
-export function agregarAlRoster(roster: number[], id: number): number[] {
-  let nuevoRoster: number[] = []; // COMPLETAR
+
+export function agregarAlRoster(roster: number[], id: number): number[] | undefined {
+  let nuevoRoster: number[] = [];
+  let n: number;
+  let contador: number = 1;
+  n =
+
+
+  while (contador <= n) {
+    console.log("Repetición número:", contador);
+    contador = contador + 1;
+
+
+   nuevoRoster.push(id);
+
+
   return nuevoRoster;
 }
 
+
 export function quitarDelRoster(roster: number[], id: number): number[] {
-  let nuevoRoster: number[] = []; // COMPLETAR
+  let nuevoRoster: number[] = [];
+
+
+   nuevoRoster.pop();
+
+
   return nuevoRoster;
 }
+
 
 cuandoPasa('filtrar', () => {
   enviarAlFrontend('jugadores', obtenerJugadoresPorIds(ids));
 });
+
 
 cuandoPasa('agregar', ({ id }: Record<string, string>) => {
   let idNumero: number = Number(id);
@@ -31,15 +67,20 @@ cuandoPasa('agregar', ({ id }: Record<string, string>) => {
   enviarAlFrontend('roster', obtenerJugadoresPorIds(roster));
 });
 
+
 cuandoPasa('quitar', ({ id }: Record<string, string>) => {
   let idNumero: number = Number(id);
   roster = quitarDelRoster(roster, idNumero);
   enviarAlFrontend('roster', obtenerJugadoresPorIds(roster));
 });
 
+
 cuandoPasa('limpiarRoster', () => {
   roster = [];
   enviarAlFrontend('roster', []);
 });
 
+
 iniciar();
+
+
